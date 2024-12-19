eead8c73dce55dc77b42b5445373e09233d6cfe2
EWongSTA <eeethan23@gmail.com>
2024-12-19 10:44:49 -0500

    methods file
    made a methods file for cpt

--- methodsCPT.java
+++ methodsCPT.java
@@ -1,27 +1,36 @@
-import arc*;
+import arc.*;
 
 public class methodsCPT{
-	public static void main(string [] args){
+	public static void main(String [] args){
 		Console con = new Console();
 	
 	
 
 	}
-		public static void logo(Console con){
+	public static void logo(Console con){
+	//title screen
 	con.println("*******  *     *  *******  *******  *******   ");
 	con.println("*        *     *  *        *        *         ");
 	con.println("*        *     *  *        *******  *******   ");
 	con.println("*   ***  *     *  *****          *        *   ");
 	con.println("*     *  *     *  *              *        *   ");
 	con.println("*******  *******  *******  *******  *******   ");
-	con.println("      *********  *      *  ********                          ");
-	con.println("          *      *      *  *                          ");
-	con.println("          *      ********  *****                            ");
-	con.println("          *      *      *  *                           ");
-	con.println("          *      *      *  ********                             ");
-	con.println("                                              ");
-	con.println("                                              ");
-	con.println("                                              ");
-	con.println("                                              ");
+	//spacing line
+	con.println(" ");
+	con.println("      *********  *      *  ********           ");
+	con.println("          *      *      *  *                  ");
+	con.println("          *      ********  *****              ");
+	con.println("          *      *      *  *                  ");
+	con.println("          *      *      *  ********           ");
+	//spacing line
+	con.println(" ");
+	con.println("     *     *  *******  ********  *******      ");
+	con.println("     *     *  *     *  *      *  *     **     ");
+	con.println("     *     *  *     *  ********  *      *     ");
+	con.println("     *  *  *  *     *  * **      *      *     ");
+	con.println("     * * * *  *     *  *   *     *     **     ");
+	con.println("     *     *  *******  *     *   *******      ");
 	
 }
+}	
+	
