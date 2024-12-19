fb740ab886a1816465cb54f1f8ba0679e25ae283
EWongSTA <eeethan23@gmail.com>
2024-12-19 10:56:24 -0500

    if statements
    added if statements based on what user inputs into strChoice

--- ICSCPT.java
+++ ICSCPT.java
@@ -11,7 +11,31 @@ public class ICSCPT{
 		TextInputFile Animals = new TextInputFile("Animals.txt");
 		
 		methodsCPT.logo(con);
+		con.sleep(3000);
 		con.clear();
+		
+		
+		//
+		String strChoice;
+		con.println("Welcome to Guess the Word!");
+		con.println("What would you like to do: Play / HighScore / Help / Quit");
+		strChoice = con.readLine();
+		
+		if (strChoice.equalsIgnoreCase("play")){
+			con.clear();
+			con.println("play");
+		}else if(strChoice.equalsIgnoreCase("highscore")){
+			con.clear();
+			con.println("highscore");
+		}else if(strChoice.equalsIgnoreCase("help")){
+			con.clear();
+			con.println("In Guess The Word, the aim of the game is to correctly guess what the word is.");
+			con.println("You get a certain amount of guesses and when you run out of guesses you lose!");
+		}else{
+			con.clear();
+			con.println("quit");
+		}
+		
 	
 	}
 }	
