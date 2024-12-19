87c84b15e9cfdb4250ccc445fd712dab877349c0
EWongSTA <eeethan23@gmail.com>
2024-12-19 10:35:04 -0500

    logo
    continued making logo

---
+++ methodsCPT.java
@@ -0,0 +1,27 @@
+import arc*;
+
+public class methodsCPT{
+	public static void main(string [] args){
+		Console con = new Console();
+	
+	
+
+	}
+		public static void logo(Console con){
+	con.println("*******  *     *  *******  *******  *******   ");
+	con.println("*        *     *  *        *        *         ");
+	con.println("*        *     *  *        *******  *******   ");
+	con.println("*   ***  *     *  *****          *        *   ");
+	con.println("*     *  *     *  *              *        *   ");
+	con.println("*******  *******  *******  *******  *******   ");
+	con.println("      *********  *      *  ********                          ");
+	con.println("          *      *      *  *                          ");
+	con.println("          *      ********  *****                            ");
+	con.println("          *      *      *  *                           ");
+	con.println("          *      *      *  ********                             ");
+	con.println("                                              ");
+	con.println("                                              ");
+	con.println("                                              ");
+	con.println("                                              ");
+	
+}
