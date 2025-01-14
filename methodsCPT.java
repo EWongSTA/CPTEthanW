bd4edbfa72c4d22d4adb18af6e8ba4350ba736be
EWongSTA <eeethan23@gmail.com>
2025-01-14 09:29:27 -0500

    bubble sort
    changed str word to str rand word

--- methodsCPT.java
+++ methodsCPT.java
@@ -54,6 +54,7 @@ public class methodsCPT{
 		TextInputFile FileLength = new TextInputFile(strFileName);
 		int intCount = 0;
 		strRandWord = new String[intCount][2];
+		strRandWord = strWord;
 		
 		while(FileLength.eof() == false){
 			FileLength.readLine();
@@ -61,15 +62,15 @@ public class methodsCPT{
 		}
 		
 		for(intRow2 = 0; intRow2 < intCount - 1; intRow2++){
-			for(intRow = 0; intRow < intCount -1 - intRow2; intRow++){
-				if(Integer.parseInt(strWord[intRow][1]) > Integer.parseInt(strWord[intRow+1][1])){
-				strTempWord = strWord[intRow][0];
-				strTempNum = strWord[intRow][1];
-				strWord[intRow][0] = strWord[intRow + 1][0];
-				strWord[intRow][1] = strWord[intRow + 1][1];
+			for(intRow = 0; intRow < intCount - 1 - intRow2; intRow++){
+				if(Integer.parseInt(strRandWord[intRow][1]) > Integer.parseInt(strRandWord[intRow+1][1])){
+				strTempWord = strRandWord[intRow][0];
+				strTempNum = strRandWord[intRow][1];
+				strRandWord[intRow][0] = strRandWord[intRow + 1][0];
+				strRandWord[intRow][1] = strRandWord[intRow + 1][1];
 				
-				strWord[intRow + 1][0] = strTempWord;
-				strWord[intRow+1][1] = strTempNum;
+				strRandWord[intRow + 1][0] = strTempWord;
+				strRandWord[intRow + 1][1] = strTempNum;
 				System.out.println(strTempWord);
 				System.out.println(strTempNum);
 				
