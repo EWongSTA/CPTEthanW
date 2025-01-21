a2dbf4755eb1a2052d581ed6c1e71021278793ac
EWongSTA <eeethan23@gmail.com>
2025-01-20 21:00:56 -0500

    bubble sort
    removed the method bubble sort because it didn't work

--- methodsCPT.java
+++ methodsCPT.java
@@ -42,42 +42,6 @@ public class methodsCPT{
 		return intRandom;
 	}
 	
-	public static String[][] BubbleSort(String[][] strWord, String strFileName){
-		// strword[0] = playable word
-		// strWord[1} = random number
-		String[][] strRandWord;
-		String strTempWord;
-		String strTempNum;
-		int intRand;
-		int intRow2;
-		int intRow;
-		TextInputFile FileLength = new TextInputFile(strFileName);
-		int intCount = 0;
-		strRandWord = new String[intCount][2];
-		strRandWord = strWord;
-		
-		while(FileLength.eof() == false){
-			FileLength.readLine();
-			intCount = intCount + 1;
-		}
-		
-		for(intRow2 = 0; intRow2 < intCount - 1; intRow2++){
-			for(intRow = 0; intRow < intCount - 1 - intRow2; intRow++){
-				if(Integer.parseInt(strRandWord[intRow][1]) > Integer.parseInt(strRandWord[intRow+1][1])){
-				strTempWord = strRandWord[intRow][0];
-				strTempNum = strRandWord[intRow][1];
-				strRandWord[intRow][0] = strRandWord[intRow + 1][0];
-				strRandWord[intRow][1] = strRandWord[intRow + 1][1];
-				
-				strRandWord[intRow + 1][0] = strTempWord;
-				strRandWord[intRow + 1][1] = strTempNum;
-				System.out.println(strTempWord);
-				System.out.println(strTempNum);
-				
-				}
-			}
-		}
-		return strRandWord;
-	}
+
 }	
 	
